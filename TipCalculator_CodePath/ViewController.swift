ecd191ff42ba560c445a8403dcf9d170cfbb9838
Narasimhan, Sindhuja <sindhujn@f45c89aa005d.sfo13.amazon.com>
2016-09-09 14:37:24 -0700

    Initial Commit

---
+++ TipCalculator_CodePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator_CodePath
+//
+//  Created by Narasimhan, Sindhuja on 9/9/16.
+//  Copyright © 2016 Narasimhan, Sindhuja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
