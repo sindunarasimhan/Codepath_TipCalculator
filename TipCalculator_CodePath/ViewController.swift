45d4e03219a36cd8d7f4492b6d6b3e2cb81a051f
Narasimhan <sindhujn@f45c89aa005d.ant.amazon.com>
2016-09-12 13:18:25 -0700

    Adding Gif path to Readme

--- TipCalculator_CodePath/ViewController.swift
+++ TipCalculator_CodePath/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalValue: UILabel!
+    @IBOutlet weak var tipValue: UILabel!
+    @IBOutlet weak var billText: UITextField!
+    
+    @IBOutlet weak var grandTotal: UILabel!
+    @IBOutlet weak var splitText: UITextField!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipArray = [0.15,0.2,0.3]
+        let billValue = Double(billText.text!) ?? 0
+        let splitValue = Double(splitText.text!) ?? 0
+
+
+        let tip = billValue * tipArray[tipPercent.selectedSegmentIndex]
+        
+        let grandTotalVal = billValue + (billValue * (tipArray[tipPercent.selectedSegmentIndex]))
+        
+        let total = (billValue/splitValue) + (tip/splitValue)
+        tipValue.text = String(format: "$%.2f",tip)
+        grandTotal.text = String(format:"$%.2f",grandTotalVal)
+        totalValue.text = String(format:"$%.2f",total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.billText.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
